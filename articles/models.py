04adbbabb2da7b98905e702dd7065dd3632658f0
gladiator <mohsen.ebrahimi@outlook.com>
2021-08-06 15:55:09 +0430

    Initial commit

---
+++ articles/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField()
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    def snipped(self):
+        if len(self.body) > 50:
+            return self.body[:50] + " ..."
+        else:
+            return self.body
